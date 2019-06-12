9450ffd5e48faa412b46bd7fe99613ddb66386e6
crvernon <chris.vernon@pnnl.gov>
2019-06-11 23:11:23 -0600

    updating formatting

--- spatial_downscale/downscale_projection.py
+++ spatial_downscale/downscale_projection.py
@@ -9,29 +9,43 @@ import spatial_downscale.downscale_utilities as pdm
 
 
 def pop_projection(urb_pop_init_year, rur_pop_init_year, mask_raster, ssp_dataFn,
-                   params_file, region_code, ssp_code, point_coors, datadir_output):
+                   params_file, region_code, ssp_code, point_coors, datadir_output,
+                   point_indices, urb_pop_snd_year):
+    """ ?
 
+    :param urb_pop_init_year:               ?
+    :param rur_pop_init_year:               ?
+    :param mask_raster:                     ?
+    :param ssp_dataFn:                      ?
+    :param params_file:                     ?
+    :param region_code:                     ?
+    :param ssp_code:                        ?
+    :param point_coors:                     ?
+    :param datadir_output:                  ?
+    :param point_indices:                   ?
+    :param urb_pop_snd_year:                ?
 
-    sub_name = "<pop_projection> "
-    print
-    sub_name + "enter"
+    :return:
+
+    """
+    print("Projection:  Begin")
 
     # Define local variables
     current_timestep = [int(s) for s in urb_pop_init_year.split("/")[-1][:-4].split("_") if s.isdigit()][0] + 10
-    time_one_data = {}  ##Dictionary storing the base year population grids
-    population_1st = {}  ##Dictionary storing the base year population arrays
-    time_one_data['Rural'] = rur_pop_init_year  ##Rural
-    time_one_data['Urban'] = urb_pop_init_year  ##Urban
-    final_arrays = {}  ##Dictionary containing final projected arrays
+    time_one_data = {}  # Dictionary storing the base year population grids
+    population_1st = {}  # Dictionary storing the base year population arrays
+    time_one_data['Rural'] = rur_pop_init_year  # Rural
+    time_one_data['Urban'] = urb_pop_init_year  # Urban
+    final_arrays = {}  # Dictionary containing final projected arrays
     final_raster = datadir_output + region_code + "_1km_" + ssp_code + "_Total_" + str(current_timestep) + ".tif"
 
-    ##Populate the array containing mask values for all points
+    # populate the array containing mask values for all points
     points_mask = pdm.raster_to_array(mask_raster)
 
-    # All indices
+    # all indices
     all_indices = pdm.all_index_retriever(urb_pop_snd_year, ["row", "column"])
 
-    # Read indices of points that fall within the state boundary
+    # read indices of points that fall within the state boundary
     with open(point_indices, 'r') as r:
         within_indices = simplejson.load(r)
 
@@ -41,43 +55,44 @@ def pop_projection(urb_pop_init_year, rur_pop_init_year, mask_raster, ssp_dataFn
 
     # Read historical urban and rural population grids into arrrays
     for setting in time_one_data:
-        ##Create the dictionary containing population of each point in year 1
+        # create the dictionary containing population of each point in year 1
         population_1st[setting] = pdm.raster_to_array(time_one_data[setting])
 
-        # Create an array containing total population values in the first historical year
+    # create an array containing total population values in the first historical year
     total_population_1st = population_1st["Rural"] + population_1st["Urban"]
 
-    # Number of columns of population grid required to derive linear indices
+    # number of columns of population grid required to derive linear indices
     ind_diffs = dist_matrix["ind_diff"].values
 
-    # Distances between current point and its close points
+    # distances between current point and its close points
     ini_dist = dist_matrix["dis"].values / 1000.0
 
-    # Downscale population projection
+    # downscale population projection
     for setting in time_one_data:
 
-        ##Define required parameters within the loop
-        pop_estimates = np.zeros(len(within_indices))  ##Population estimates for the second year
+        # TODO: See why these are not used
+        # define required parameters within the loop
+        pop_estimates = np.zeros(len(within_indices))  # Population estimates for the second year
         suitability_estimates = deque()  # Suitability estimates in the second year
 
-        ##Output raster
+        # output raster
         output = datadir_output + region_code + "_1km_" + ssp_code + "_" + setting + "_" + str(
             current_timestep) + ".tif"
 
-        ##Calculate aggregate urban/rural population at time 1
+        # calculate aggregate urban/rural population at time 1
         pop_first_year = population_1st[setting][within_indices]
         pop_t1 = pop_first_year.sum()
 
-        ##Load the SSP file to retrieve the aggregated projected population at time 2 for downscaling
+        # load the SSP file to retrieve the aggregated projected population at time 2 for downscaling
         ssp_data = pd.read_csv(ssp_dataFn)
-        if (setting == "Urban"):
+        if setting == "Urban":
             pop_t2 = ssp_data.loc[(ssp_data["Year"] == current_timestep) & (ssp_data["Scenario"] == ssp_code),
                                   "UrbanPop"].values[0]
         else:
             pop_t2 = ssp_data.loc[(ssp_data["Year"] == current_timestep) & (ssp_data["Scenario"] == ssp_code),
                                   "RuralPop"].values[0]
 
-            ##Population change between years 1 and 2
+        # population change between years 1 and 2
         pop_change = pop_t2 - pop_t1
 
         if pop_change < 0:
@@ -85,9 +100,9 @@ def pop_projection(urb_pop_init_year, rur_pop_init_year, mask_raster, ssp_dataFn
         else:
             negative_mod = 0
 
-        # Extract the alpha and beta values from the calibration files
+        # extract the alpha and beta values from the calibration files
         calib_params = pd.read_csv(params_file)
-        if (setting == "Urban"):
+        if setting == "Urban":
             calib_params = calib_params.loc[calib_params.SSP == ssp_code, ["Alpha_Urban", "Beta_Urban"]].values
         else:
             calib_params = calib_params.loc[calib_params.SSP == ssp_code, ["Alpha_Rural", "Beta_Rural"]].values
@@ -98,80 +113,82 @@ def pop_projection(urb_pop_init_year, rur_pop_init_year, mask_raster, ssp_dataFn
         dist = -b * ini_dist
         exp_xx_inv_beta_dist = np.exp(dist)
 
-        # Initialize the parallelization
+        # initialize the parallelization
         pool = Pool(processes=multiprocessing.cpu_count())
 
-        # Provide the inputs for the parallelized function
+        # provide the inputs for the parallelized function
         parallel_elements = deque([(i, ind_diffs, total_population_1st, a, exp_xx_inv_beta_dist)
                                    for i in within_indices])
 
-        # Derive suitability estimates
+        # derive suitability estimates
         suitability_estimates = pool.map(pdm.suitability_estimator, parallel_elements)
 
-        # Change suitability estimates to a numpy array
+        # change suitability estimates to a numpy array
         suitability_estimates = np.array(suitability_estimates)
 
-        # Exract only the necessary mask values that fall within the state boundary
+        # exract only the necessary mask values that fall within the state boundary
         cur_points_mask = points_mask[within_indices]
 
-        ##In case of population decline, suitability estimates are reciprocated
+        # in case of population decline, suitability estimates are reciprocated
         if negative_mod:
 
             # find those whose mask is 0 but have population, they should decline anyway
             mask_zero = np.where(cur_points_mask == 0)[0]
             pop_non_zero = np.where(pop_first_year != 0)[0]
 
-            # Those cells with mask value of zero and population are the intersection of the two above arrays
+            # those cells with mask value of zero and population are the intersection of the two above arrays
             pop_mask = np.intersect1d(mask_zero, pop_non_zero, assume_unique=True)
 
-            # Change the mask value of the above cells to 1 so that they also lose population
+            # change the mask value of the above cells to 1 so that they also lose population
             cur_points_mask[pop_mask] = cur_points_mask.mean()
 
-            # Adjust suitability values by applying mask values
+            # adjust suitability values by applying mask values
             suitability_estimates = cur_points_mask * suitability_estimates
 
-            # Inverse current mask values for a better reflection of population decline
+            # inverse current mask values for a better reflection of population decline
             suitability_estimates[suitability_estimates != 0] = 1.0 / suitability_estimates[suitability_estimates != 0]
 
         else:
-            ##Adjust suitability values by applying its mask values
+            # adjust suitability values by applying its mask values
             suitability_estimates = cur_points_mask * suitability_estimates
 
-        ##Total suitability for the whole area, which is the summation of all individual suitability values
+        # total suitability for the whole area, which is the summation of all individual suitability values
         tot_suitability = suitability_estimates.sum()
 
-        ##Final population estimates if nagative mode is off
+        # tinal population estimates if nagative mode is off
         pop_estimates = suitability_estimates / tot_suitability * pop_change + pop_first_year
 
-        # Adjust the projection so that no cell can have less than 0 individuals.
+        # adjust the projection so that no cell can have less than 0 individuals.
         if negative_mod:
-            ##To make sure that there is no negative population
-            while any(pop < 0 for pop in pop_estimates):
-                new_tot_suitability = 0  ##Total suitability calculated over points with positive population
-                extra_pop_mod = 0  ##For adjusting negative population values
 
-                ##Treating negative population values
+            # to make sure that there is no negative population
+            while any(pop < 0 for pop in pop_estimates):
+
+                # TODO:  find out why these are not used
+                new_tot_suitability = 0  # Total suitability calculated over points with positive population
+                extra_pop_mod = 0  # For adjusting negative population values
+
+                # treating negative population values
                 extra_pop_mod = abs(pop_estimates[pop_estimates < 0].sum())
                 pop_estimates[pop_estimates < 0] = 0
 
-                ##Calculate the new total suitability value based on points with positive projected population
+                # calculate the new total suitability value based on points with positive projected population
                 new_tot_suitability = suitability_estimates[pop_estimates > 0].sum()
 
-                ##Adjust non-negative population values to maintain the total aggregated population
+                # adjust non-negative population values to maintain the total aggregated population
                 pop_estimates[pop_estimates > 0] = pop_estimates[pop_estimates > 0] - (
                             suitability_estimates[pop_estimates > 0] / new_tot_suitability) * extra_pop_mod
 
-        # Save the projection array of the current setting
+        # save the projection array of the current setting
         final_arrays[setting] = pop_estimates
 
-        # Save the final urban/rural raster
-        print
-        sub_name + "output downscaled data: " + output
+        # save the final urban, rural raster
+        print("Saving urban and rural raster to:  {}".format(output))
         pdm.array_to_raster(mask_raster, pop_estimates, within_indices, output)
 
-    # Calculate the total population array
+    # calculate the total population array
     total_array = final_arrays["Rural"] + final_arrays["Urban"]
-    # Save the final total raster
-    print
-    sub_name + "output downscaled data: " + final_raster
+
+    # save the final total raster
+    print("Saving total population raster to:  {}".format(final_raster))
     pdm.array_to_raster(mask_raster, total_array, within_indices, final_raster)
