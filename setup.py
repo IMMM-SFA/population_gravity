5e21b8e23ec9518745779774e99347c643afdd15
crvernon <chrisrvernon@gmail.com>
2020-08-05 08:02:35 -0400

    add package data install in setup

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     url='https://github.com/IMMM-SFA/population_gravity',
+    include_package_data=True,
     license='BSD 2-Clause',
     author='Hamidreza Zoraghein, Chris R. Vernon',
     author_email='hzoraghein@popcouncil.org, chris.vernon@pnnl.gov',
